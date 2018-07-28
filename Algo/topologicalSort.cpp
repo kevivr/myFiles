764970ba8ea3cd0079d285092c0a157cd8a282eb
kevivr <40536171+kevivr@users.noreply.github.com>
2018-07-28 03:54:52 -0400

    Create topologicalSort.cpp

---
+++ Algo/topologicalSort.cpp
@@ -0,0 +1,16 @@
+int fg, v[..];
+int od[..], p; // the final order
+void dfs(int a) {
+if(v[a]==1) fg = 1;
+if(v[a]) return;
+v[a] = 1; // gray
+for each neighbor a->i dfs(i);
+v[a] = 2; // black
+od[p] = a; p--;
+}
+...
+for(i=0; i<n; i++) v[i]=0; //white
+fg=0; p=n-1;
+for(i=0; i<n; i++) if(!v[i])
+dfs(i);
+if(fg) say there is a cycle;
